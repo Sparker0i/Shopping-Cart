fb97e2c33629eec48a76e0834746a38588a8821e
Aaditya Menon <aaditya@sparker0i.me>
2018-12-23 20:07:17 +0530

    Server first build

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var app = express()
+
+app.use('/public' , express.static(__dirname + 'public'));
+app.use('node_modules' , express.static(__dirname + 'node_modules'))
+
+app.listen(9999 , function() {
+    console.log("Server Running at Port 9999")
+})
+
+app.get('/' , function(req , res) {
+    res.sendFile('main.html' , {'root' : __dirname + '/public'})
+})
